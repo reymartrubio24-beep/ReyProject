9b52103f70fcbe883c1b2f51033312df3044d030
Reyy <reymartrubio24@gmail.com>
2025-10-27 19:20:21 +0800

    exploring java

---
+++ Day4.java
@@ -0,0 +1,36 @@
+ public class Day4 {  //how to loop 
+     public static void main(String[] args) {
+         int r = 50; 
+         while (r < 100) {
+             System.out.println(r);
+             r ++;
+            
+        }
+     }
+ }
+
+
+// public class Day4 {  // do\while looping ni sya kung unsa im
+//            public static void main(String[] args) {
+             
+//             int y = 5;  //Variable Initialization
+//             do {              // 
+//                 System.out.println(y);
+//                 y++; // if dili nimu ma butang ang incrementation kay dili mo stop iyang loop
+
+//             }
+//             while (y < 10);
+//            }
+    
+// }
+
+
+
+// // for loop para dali sya/ short sa while loop ug 
+// public class Day4 {
+
+//     public static void main(String[] args) {
+//          for (int i = 1; i <= 5; i++) {
+//             System.out.println(i);         }
+//     }
+// }
